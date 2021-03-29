b3c5f692e3ea62b628c117fefeef88ee8dad4344
Kayanr <kayan.reid@gmail.com>
2021-03-29 13:46:56 -0400

    add checkGameBoard function
    add checkGameBoard function

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ function place(box) {
   if (box.innerText != "") return;
   box.innerText = currentPlayer;
   
-  currentPlayer == "O" ? currentPlayer = "X": currentPlayer = "O";
+    currentPlayer == "O" ? currentPlayer = "X" : currentPlayer = "O";
+    checkGameBoard();
   }
-}
+
+function checkGameBoard() {
+    let first = document.getElementById("0_0").innerText;
+    let second = document.getElementById("1_0").innerText;
+    let third = document.getElementById("2_0").innerText;
+
+    if (first == second && first == third) {
+        alert("Winner!");
+    }
+  }
